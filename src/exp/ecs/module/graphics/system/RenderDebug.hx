dd941f917636364a261d254a8b653e1e15ed0160
Kevin Leung <kevinresol@gmail.com>
2020-10-26 23:21:59 +0800

    Render debug info

--- src/exp/ecs/module/graphics/system/RenderDebug.hx
+++ src/exp/ecs/module/graphics/system/RenderDebug.hx
@@ -3,15 +3,16 @@ package exp.ecs.module.graphics.system;
 import kha.Scheduler;
 
 /**
- * Render FPS on screen
+ * Render debug info on screen
  */
 @:nullSafety(Off)
-class RenderFps extends System {
+class RenderDebug extends System {
 	public var frame:kha.Framebuffer;
 	public var font:kha.Font;
 	public var x:Int;
 	public var y:Int;
 
+	var count:Int;
 	var time = Scheduler.time();
 
 	public function new(font, x, y) {
@@ -20,11 +21,15 @@ class RenderFps extends System {
 		this.y = y;
 	}
 
+	override function initialize(world:World) {
+		return tink.state.Observable.auto(world.entities.count).bind(count -> this.count = count);
+	}
+
 	override function update(dt:Float) {
 		final g2 = frame.g2;
 
 		final now = Scheduler.time();
-		final fps = 'FPS: ${Math.round(1 / (now - time))}';
+		final fps = 'Entity Count: $count';
 		time = now;
 
 		g2.begin(false);
